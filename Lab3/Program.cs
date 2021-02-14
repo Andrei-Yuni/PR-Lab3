15d8ac5af868ba48c212773d34e48b2e4779cdfe
killerart <442234@mail.ru>
2021-02-14 18:43:15 +0200

    added content printing

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -7,39 +7,38 @@ using System.Threading.Tasks;
 
 namespace Lab3
 {
-     class Program
-     {
-          static async Task Main(string[] args)
-          {
-               var host = "http://prlab3-test.eu-central-1.elasticbeanstalk.com";
-               var httpClient = new HttpClient{ BaseAddress = new Uri(host) };
+    class Program
+    {
+        static async Task Main(string[] args) {
+            var host = "http://prlab3-test.eu-central-1.elasticbeanstalk.com";
+            var httpClient = new HttpClient { BaseAddress = new Uri(host) };
 
-               Console.WriteLine("POST:");
-               var postContent = JsonContent.Create(new
-               {
-                    username = "Kek",
-                    password = "cheburek"
-               });
-               await httpClient.PostAsync("/api/Auth/register", postContent);
-               var result = await httpClient.PostAsync("/api/Auth/login", postContent);
-               Console.WriteLine(result);
-               Console.WriteLine();
-               string cookies = result.Headers.GetValues("Set-Cookie").First();
-               httpClient.DefaultRequestHeaders.Add("Cookie", cookies);
+            Console.WriteLine("POST:");
+            var postContent = JsonContent.Create(new {
+                username = "Kek",
+                password = "cheburek"
+            });
+            await httpClient.PostAsync("/api/Auth/register", postContent);
+            var result = await httpClient.PostAsync("/api/Auth/login", postContent);
+            var cookies = result.Headers.GetValues("Set-Cookie");
+            httpClient.DefaultRequestHeaders.Add("Cookie", cookies);
+            Console.WriteLine(result);
+            Console.WriteLine();
 
-               Console.WriteLine("GET:");
-               result = await httpClient.GetAsync("/api/Users");
-               Console.WriteLine(result);
-               Console.WriteLine();
+            Console.WriteLine("GET:");
+            result = await httpClient.GetAsync("/api/Users");
+            Console.WriteLine(result);
+            Console.WriteLine($"Content: {await result.Content.ReadAsStringAsync()}");
+            Console.WriteLine();
 
-               Console.WriteLine("OPTIONS:");
-               result = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Options, "/api/Users"));
-               Console.WriteLine(result);
-               Console.WriteLine();
+            Console.WriteLine("OPTIONS:");
+            result = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Options, "/api/Users"));
+            Console.WriteLine(result);
+            Console.WriteLine();
 
-               Console.WriteLine("HEAD:");
-               result = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, "/api/Users"));
-               Console.WriteLine(result);
-          }
-     }
+            Console.WriteLine("HEAD:");
+            result = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, "/api/Users"));
+            Console.WriteLine(result);
+        }
+    }
 }
