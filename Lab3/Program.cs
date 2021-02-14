87317f70a591dc128996b02b4a48617b5e74a4dd
Yunosti <kerokar4@gmail.com>
2021-02-14 15:11:27 +0200

    Add project files.

---
+++ Lab3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+     class Program
+     {
+          static async Task Main(string[] args)
+          {
+               var host = "http://prlab3-test.eu-central-1.elasticbeanstalk.com";
+               var httpClient = new HttpClient{ BaseAddress = new Uri(host) };
+
+               Console.WriteLine("POST:");
+               var postContent = JsonContent.Create(new
+               {
+                    username = "Kek",
+                    password = "cheburek"
+               });
+               await httpClient.PostAsync("/api/Auth/register", postContent);
+               var result = await httpClient.PostAsync("/api/Auth/login", postContent);
+               Console.WriteLine(result);
+               Console.WriteLine();
+               string cookies = result.Headers.GetValues("Set-Cookie").First();
+               httpClient.DefaultRequestHeaders.Add("Cookie", cookies);
+
+               Console.WriteLine("GET:");
+               result = await httpClient.GetAsync("/api/Users");
+               Console.WriteLine(result);
+               Console.WriteLine();
+
+               Console.WriteLine("OPTIONS:");
+               result = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Options, "/api/Users"));
+               Console.WriteLine(result);
+               Console.WriteLine();
+
+               Console.WriteLine("HEAD:");
+               result = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, "/api/Users"));
+               Console.WriteLine(result);
+          }
+     }
+}
